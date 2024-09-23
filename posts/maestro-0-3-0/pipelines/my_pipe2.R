afa2ab660a1549f37d0d85ba4062431602f79fe1
Will Hipson <will.hipson@hiaa.ca>
2024-09-23 12:30:05 -0300

    maestro 0.3.0 release

---
+++ posts/maestro-0-3-0/pipelines/my_pipe2.R
@@ -0,0 +1,11 @@
+#' my_pipe2 maestro pipeline
+#'
+#' @maestroFrequency 1 week
+#' @maestroStartTime 2024-09-23
+#' @maestroTz UTC
+#' @maestroLogLevel INFO
+
+my_pipe2 <- function() {
+
+  # Pipeline code
+}
---
+++ posts/maestro-0-3-0/pipelines/my_pipe3.R
@@ -0,0 +1,11 @@
+#' my_pipe3 maestro pipeline
+#'
+#' @maestroFrequency 2 hours
+#' @maestroStartTime 2024-09-23
+#' @maestroTz UTC
+#' @maestroLogLevel INFO
+
+my_pipe3 <- function() {
+
+  # Pipeline code
+}
